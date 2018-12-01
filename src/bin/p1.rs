f18d7c85463dfff18c05a6210715d04634600269
Mike Sampson <mike@sambodata.com>
2018-12-01 18:19:08 +1100

    puzzle one

--- src/bin/p1.rs
+++ src/bin/p1.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+extern crate failure;
+
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::collections::HashSet;
+use failure::Error;
+
+fn main() -> Result<(), Error> {
+    let file = BufReader::new(File::open("data/p1.txt")?);
+
+    let mut data = vec![];
+    for line in file.lines() {
+        data.push(line?.parse()?);
+    }
+
+    let total: i64 = data.iter().sum();
+    println!("Part 1: {}", total);
+
+    let mut total = 0;
+    let mut freqs = HashSet::new();
+
+    for n in data.iter().cycle() {
+        total += n;
+        if freqs.contains(&total) {
+            break;
+        }
+        freqs.insert(total);
+    }
+
+    println!("Part 2: {}", total);
+
+    Ok(())
 }
