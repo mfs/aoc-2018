258d29f321ad305f981efb5ec310c9614671fcf1
Mike Sampson <mike@sambodata.com>
2018-12-09 18:00:54 +1100

    puzzle nine
    The test data worked first try though the actual data uncovered a bug in
    the rotate 7 part of my code. Eventually just used a loop to do this.
    I did part one using a vec and keeping a current index. This worked
    though became unfeasible for part two. Switched to a deque and kept the
    current index at the beginning. This is still not the best performance
    wise though was good enough.

---
+++ src/bin/p9.rs
@@ -0,0 +1,49 @@
+extern crate failure;
+
+use std::collections::VecDeque;
+use failure::{Error, err_msg};
+
+fn process(num_marbles: usize, num_players: usize) -> Result<usize, Error> {
+
+    let mut current_player = 0;
+    let mut players = vec![0; num_players];
+    let mut circle = VecDeque::new();
+    circle.push_back(0);
+
+    for x in 1..num_marbles {
+        if x % 23 == 0 {
+            players[current_player] += x;
+            for _ in 0..7 {
+                let last = circle.pop_back().ok_or(err_msg("empty buffer"))?;
+                circle.push_front(last);
+            }
+
+            players[current_player] += circle.pop_front().ok_or(err_msg("empty buffer"))?;
+        } else {
+            for _ in 0..2 {
+                let first = circle.pop_front().ok_or(err_msg("empty buffer"))?;
+                circle.push_back(first);
+            }
+            circle.push_front(x);
+        }
+
+        current_player = (current_player + 1) % num_players;
+    }
+
+   Ok(*players.iter().max().ok_or(err_msg("empty scores"))?)
+}
+
+
+fn main() -> Result<(), Error> {
+
+    const NUM_MARBLES: usize = 72060;
+    const NUM_PLAYERS: usize = 410;
+
+    let part1 = process(NUM_MARBLES, NUM_PLAYERS)?;
+    println!("Part 1: {}", part1);
+
+    let part2 = process(NUM_MARBLES * 100, NUM_PLAYERS)?;
+    println!("Part 2: {}", part2);
+
+    Ok(())
+}
