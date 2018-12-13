6e8aba0e75779f1f53f2303b5a2b718a842d08a0
Mike Sampson <mike@sambodata.com>
2018-12-13 22:11:20 +1100

    puzzle thirteen
    Made so many stupid mistakes with this one. Initially only checked for
    collisions at the end of a tick which caused me to miss a collision.
    Eventually got it. Code could use a clean up.
    
    I still need to push puzzle twelve. Unfortunately while cleaning it up I
    broke part two. No idea what I have done. One for the weekend.

---
+++ src/bin/p13.rs
@@ -0,0 +1,174 @@
+extern crate failure;
+
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use failure::Error;
+
+#[derive(Eq, PartialEq, Ord, PartialOrd, Debug)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn left(&self) -> Self {
+        match self {
+            Direction::Up => Direction::Left,
+            Direction::Down => Direction::Right,
+            Direction::Left => Direction::Down,
+            Direction::Right => Direction::Up,
+        }
+    }
+    fn right(&self) -> Self {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+            Direction::Right => Direction::Down,
+        }
+    }
+}
+
+
+#[derive(Eq, PartialEq, Ord, PartialOrd, Debug)]
+enum Turn {
+    Left,
+    Straight,
+    Right,
+}
+
+impl Turn {
+    fn next(&self) -> Self {
+        match self {
+            Turn::Left => Turn::Straight,
+            Turn::Straight => Turn::Right,
+            Turn::Right => Turn::Left,
+        }
+    }
+}
+
+#[derive(Eq, PartialEq, Ord, PartialOrd, Debug)]
+struct Cart {
+    y: usize,
+    x: usize,
+    direction: Direction,
+    next_turn: Turn,
+    id: u64,
+    is_alive: bool,
+}
+
+fn main() -> Result<(), Error> {
+    let  file = BufReader::new(File::open("data/p13.txt")?);
+
+    let mut grid: Vec<Vec<char>> = vec![];
+
+    for line in file.lines() {
+        let line = line?;
+
+        grid.push(line.chars().collect());
+    }
+
+    let mut carts = vec![];
+    let mut id = 0;
+
+    for y in 0..150 {
+        for x in 0..150 {
+            let c = grid[y][x];
+            let square = match c {
+                '^' => Some(Cart {x: x, y: y, direction: Direction::Up, next_turn: Turn::Left, id: id, is_alive: true }),
+                'v' => Some(Cart {x: x, y: y, direction: Direction::Down, next_turn: Turn::Left, id: id, is_alive: true }),
+                '<' => Some(Cart {x: x, y: y, direction: Direction::Left, next_turn: Turn::Left, id: id, is_alive: true }),
+                '>' => Some(Cart {x: x, y: y, direction: Direction::Right, next_turn: Turn::Left, id: id, is_alive: true }),
+                _   => None,
+            };
+
+            if let Some(cart) = square {
+                let p = match cart.direction {
+                    Direction::Up => '|',
+                    Direction::Down => '|',
+                    Direction::Left => '-',
+                    Direction::Right => '-',
+                };
+                grid[y][x] = p;
+                carts.push(cart);
+                id += 1;
+            }
+
+        }
+    }
+
+    let num_carts = carts.len();
+    let mut collisions = vec![];
+
+    'outer: loop {
+        carts.sort();
+        for i in 0..carts.len() {
+            // move cart
+            match carts[i].direction {
+                Direction::Up => { carts[i].y -= 1 },
+                Direction::Down => { carts[i].y += 1 },
+                Direction::Left => { carts[i].x -= 1 },
+                Direction::Right => { carts[i].x += 1 },
+            }
+
+            // check collisions
+            for j in 0..carts.len() {
+                if carts[i].x == carts[j].x && carts[i].y == carts[j].y && i != j && carts[j].is_alive && carts[i].is_alive {
+                    if collisions.is_empty() {
+                        println!("Part 1: {} {}", carts[i].x, carts[j].y);
+                    }
+                    collisions.push((carts[i].x, carts[i].y));
+                    carts[i].is_alive = false;
+                    carts[j].is_alive = false;
+                }
+            }
+
+
+            // adjust direction corners, intersections
+            let c = grid[carts[i].y][carts[i].x];
+
+            match c {
+                '+' => {
+                    match carts[i].next_turn {
+                        Turn::Left => { carts[i].direction = carts[i].direction.left() },
+                        Turn::Right => { carts[i].direction = carts[i].direction.right() },
+                        Turn::Straight => {},
+                    }
+                    carts[i].next_turn = carts[i].next_turn.next();
+                },
+                '/' => {
+                    carts[i].direction = match carts[i].direction {
+                        Direction::Up => Direction::Right,
+                        Direction::Down => Direction::Left,
+                        Direction::Left => Direction::Down,
+                        Direction::Right => Direction::Up,
+                    };
+                },
+                '\\' => {
+                    carts[i].direction = match carts[i].direction {
+                        Direction::Up => Direction::Left,
+                        Direction::Down => Direction::Right,
+                        Direction::Left => Direction::Up,
+                        Direction::Right => Direction::Down,
+                    };
+                },
+                _ => {},
+            }
+        }
+
+        // last survivor
+        if collisions.len() * 2 == num_carts - 1 {
+            for j in 0..carts.len() {
+                if carts[j].is_alive {
+                    println!("Part 2: {} {}", carts[j].x, carts[j].y);
+                }
+            }
+            break 'outer;
+        }
+    }
+
+    Ok(())
+}
