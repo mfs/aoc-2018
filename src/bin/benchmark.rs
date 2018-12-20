49f8b85c017c5f9d4c40fde3ad1a8c62e59eef38
Mike Sampson <mike@sambodata.com>
2018-12-20 22:22:43 +1100

    Update benchmark.rs
    Skip programs not present.

--- src/bin/benchmark.rs
+++ src/bin/benchmark.rs
@@ -8,27 +8,27 @@ use failure::Error;
 fn main() -> Result<(), Error> {
     let mut times = vec![];
 
-    let programs: Vec<_> = (1..=20).map(|n| format!("target/release/p{}", n)).collect();
+    let programs: Vec<_> = (1..=25).map(|n| format!("target/release/p{}", n)).collect();
 
-    for p in &programs {
-        if !Path::new(p).exists() {
-            break;
+    for p in programs {
+        if !Path::new(&p).exists() {
+            continue;
         }
         println!("{}", p);
         let start = time::precise_time_ns();
-        std::process::Command::new(p).status()?;
+        std::process::Command::new(&p).status()?;
         let end = time::precise_time_ns();
-        times.push(end - start);
+        times.push((p, end - start));
         println!();
     }
 
     println!("Times");
     println!("----------------------------------------");
-    for (t, p) in times.iter().zip(programs) {
+    for (p, t) in &times {
         println!("{}: {}ms", p, Duration::nanoseconds(*t as i64).num_milliseconds());
     }
 
-    let total = times.iter().sum::<u64>();
+    let total = times.iter().map(|x| x.1).sum::<u64>();
     println!("\nTotal time: {}ms", Duration::nanoseconds(total as i64).num_milliseconds());
 
     Ok(())
